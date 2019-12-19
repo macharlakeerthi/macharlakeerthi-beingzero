497a4989fa0a6fb14a204f368f93977f00776ecf
macharlakeerthi <59007919+macharlakeerthi@users.noreply.github.com>
2019-12-19 13:08:52 +0530

    static files created

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/htmls/index.html'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
